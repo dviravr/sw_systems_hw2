cb4b95b094e904c44199d3f982393877bda8074d
DavidHarlap <adavid12@gmail.com>
2020-11-23 20:59:01 +0200

    add chackBALANCE

--- myBank.c
+++ myBank.c
@@ -6,7 +6,7 @@ const int setAcountNumber = 901;
 float bank[50][2]= {{0},{0}};
 int capacity = 0;
 
-void openAcount(double amount);{
+void openAcount(double amount){
     if(capacity > 49){
         printf("We are apologies, the bank can not open new account in that moment.");
     }
@@ -14,16 +14,33 @@ void openAcount(double amount);{
     bank[capacity][0]=1;
     bank[capacity][1]=amount;
     printf("Account created successfully, your account number is: %d",(capacity+setAcountNumber));
-
     capacity++;
 
 }
+void chackBalance(int acountNumber){
+    int index = acountNumber-setAcountNumber;
+    if(bank[index][0]!=1){
+        printf("Err :This acount number dont exsist.");
+    }
+    else
+    {
+        printf("The balance of account number %d is: %f",acountNumber, bank[index][1]);
+    }
+    
+}
 
 
-void chackBalance(int acountNumber
-void deposit(int acountNumber, dou
-void withrawal(int acountNumber, d
-void closeAcount(int acountNumber)
-void addingInterest(float interest
+void deposit(int acountNumber, double anount){
+    
+
+}
+
+void withrawal(int acountNumber, double anount);
+
+void closeAcount(int acountNumber);
+
+void addingInterest(float interestRate);
+
 void printAllOpenAcounts();
+
 void closeAllAcounts();
