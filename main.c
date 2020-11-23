fd7ae3c1b6ee311cbc49dfccb48573627ae664e2
dviravr <dviravr@gmail.com>
2020-11-23 17:03:27 +0200

    fix main

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "myBank.h"
+// #include "myBank.h"
 
 int main()
 {
@@ -14,57 +14,57 @@ int main()
         scanf("%c",&command);
         switch (command)
         {
-        case 'o':
-        case 'O':
-            printf("Initial deposit?:  ");
-            scanf("%lf",&amount);
-            openAcount(amount);
-            break;
-        case 'b':
-        case 'B':
-            printf("Account number?:  ");
-            scanf("%d",&acountNumber);
-            chackBalance(acountNumber);
-            break;
-        case 'd':
-        case 'D':
-            printf("Account number?:  ");
-            scanf("%d",&acountNumber);
-            printf("Amount?:  ");
-            scanf("%lf",&amount);
-            deposit(acountNumber, amount);
-            break;    
-        case 'w':
-        case 'W':
-            printf("Account number?:  ");
-            scanf("%d",&acountNumber);
-            printf("Amount?:  ");
-            scanf("%lf",&amount);
-            deposit(acountNumber, amount);
-            break;
-        case 'c':
-        case 'C':
-            printf("Account number?:  ");
-            scanf("%d",&acountNumber);
-            closeAcount(acountNumber);
-            break;
-        case 'i':
-        case 'I':
-            printf("Interest rate?:  ");
-            scanf("%f",&interestRate);
-            addingInterest(interestRate);
-            break;                
-        case 'p':
-        case 'P':
-            printAllOpenAcounts();
-            break; 
-        case 'e':
-        case 'E':
-            closeAllAcounts();
-            condition = 0;
-            break;             
-        default:
-            printf("Invalid command, please try another command.\n");
+            case 'o':
+            case 'O':
+                printf("Initial deposit?:  ");
+                scanf("%lf",&amount);
+                openAcount(amount);
+                break;
+            case 'b':
+            case 'B':
+                printf("Account number?:  ");
+                scanf("%d",&acountNumber);
+                chackBalance(acountNumber);
+                break;
+            case 'd':
+            case 'D':
+                printf("Account number?:  ");
+                scanf("%d",&acountNumber);
+                printf("Amount?:  ");
+                scanf("%lf",&amount);
+                deposit(acountNumber, amount);
+                break;    
+            case 'w':
+            case 'W':
+                printf("Account number?:  ");
+                scanf("%d",&acountNumber);
+                printf("Amount?:  ");
+                scanf("%lf",&amount);
+                withrawal(acountNumber, amount);
+                break;
+            case 'c':
+            case 'C':
+                printf("Account number?:  ");
+                scanf("%d",&acountNumber);
+                closeAcount(acountNumber);
+                break;
+            case 'i':
+            case 'I':
+                printf("Interest rate?:  ");
+                scanf("%f",&interestRate);
+                addingInterest(interestRate);
+                break;                
+            case 'p':
+            case 'P':
+                printAllOpenAcounts();
+                break; 
+            case 'e':
+            case 'E':
+                closeAllAcounts();
+                condition = 0;
+                break;             
+            default:
+                printf("Invalid command, please try another command.\n");
         }
     }
     return 0;
