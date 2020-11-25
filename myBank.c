d22e82036bc40ea021fe8688270eb5331d94b179
DavidHarlap <adavid12@gmail.com>
2020-11-25 20:06:27 +0200

    merge

--- myBank.c
+++ myBank.c
@@ -1,23 +1,12 @@
 #include "myBank.h"
 #include <stdio.h>
 
-<<<<<<< HEAD
-const int SET_ACOUNT_NUMBER = 901;
-float bank[50][2] = {{0}, {0}};
-int capacity = 0;
-
-void openAcount(double amount)
-{
-    if (capacity > 49)
-    {
-=======
 const int SET_ACCOUNT_NUMBER = 901;
 float bank[50][2]= {{0},{0}};
 int capacity = 0;
 
 void openAccount(double amount){
     if(capacity > 49){
->>>>>>> 9ec81f2381bf461296a4ff0a71bd348e45c4bbc3
         printf("We are apologies, the bank can not open new account in that moment.\n");
         return;
     }
@@ -35,34 +24,6 @@ void openAccount(double amount){
             break;
         }
     }
-<<<<<<< HEAD
-
-    bank[temp][0] = 1;
-    bank[temp][1] = amount;
-    printf("Account created successfully, your account number is: %d\n", (temp + SET_ACOUNT_NUMBER));
-    capacity++;
-}
-
-void chackBalance(int acountNumber)
-{
-    int index = acountNumber - SET_ACOUNT_NUMBER;
-    if (bank[index][0] == 0)
-    {
-        printf("Err :This acount number dont exsist.\n");
-    }
-    else
-    {
-        printf("The balance of account number %d is: %.2f\n", acountNumber, bank[index][1]);
-    }
-}
-
-void withrawal(int acountNumber, double amount)
-{
-    int index = acountNumber - SET_ACOUNT_NUMBER;
-    if (bank[index][0] == 0)
-    {
-        printf("Err: This acount number dont exsist.\n");
-=======
     
     bank[temp][0]=1;
     bank[temp][1]=amount;
@@ -86,7 +47,6 @@ void withdrawal(int accountNumber, double amount){
     int index = accountNumber-SET_ACCOUNT_NUMBER;
     if(bank[index][0]==0){
         printf("Err: This account number dont exsist.\n");
->>>>>>> 9ec81f2381bf461296a4ff0a71bd348e45c4bbc3
     }
     else if (bank[index][1] < amount)
     {
@@ -95,64 +55,18 @@ void withdrawal(int accountNumber, double amount){
     else
     {
         bank[index][1] -= amount;
-<<<<<<< HEAD
-        printf("The money withdrawal was successful! The updated amount of account number %d is %.2f.\n", acountNumber, bank[index][1]);
-=======
-        printf("The money withdrawal was successful! The updated amount of account number %d is %.2f.\n",accountNumber,bank[index][1]);
->>>>>>> 9ec81f2381bf461296a4ff0a71bd348e45c4bbc3
-    }
-}
 
-<<<<<<< HEAD
-void deposit(int acountNumber, double amount)
-{
-    int index = acountNumber - SET_ACOUNT_NUMBER;
-    if (bank[index][0] == 0)
-    {
-        printf("Err: This acount number dont exsist.\n");
-=======
+
 void deposit(int accountNumber, double amount){
     int index = accountNumber-SET_ACCOUNT_NUMBER;
     if(bank[index][0]==0){
         printf("Err: This account number dont exsist.\n");
->>>>>>> 9ec81f2381bf461296a4ff0a71bd348e45c4bbc3
     }
     else
     {
         bank[index][1] += amount;
-<<<<<<< HEAD
-        printf("The deposit was made successfully! The updated amount of account number %d is %.2f. \n", acountNumber, bank[index][1]);
-=======
         printf("The deposit was made successfully! The updated amount of account number %d is %.2f. \n",accountNumber,bank[index][1]);
     }
->>>>>>> 9ec81f2381bf461296a4ff0a71bd348e45c4bbc3
-    }
-}
-
-<<<<<<< HEAD
-void closeAcount(int acountNumber)
-{
-    int index = acountNumber - SET_ACOUNT_NUMBER;
-    if (bank[index][0] == 0)
-    {
-        printf("Err: This acount number dont exsist.\n");
-    }
-    else
-    {
-        bank[index][0] = 0;
-        printf("Account number %d successfully closed!\n", acountNumber);
-=======
-void closeAccount(int accountNumber){
-    int index = accountNumber-SET_ACCOUNT_NUMBER;
-    if(bank[index][0]==0){
-        printf("Err: This account number dont exsist.\n");
-    }
-    else
-    {
-        bank[index][0]= 0;
-        printf("Account number %d successfully closed!\n", accountNumber);
->>>>>>> 9ec81f2381bf461296a4ff0a71bd348e45c4bbc3
-        capacity--;
     }
 }
 
@@ -165,34 +79,12 @@ void addingInterest(float interestRate)
     }
     float per = 1 + (interestRate / 100.0);
     for (size_t i = 0; i < 50; i++)
-    {
         if (bank[i][0] != 0)
             bank[i][1] *= per;
     }
     printf("The interest rate was added successfully!\n");
 }
 
-<<<<<<< HEAD
-void printAllOpenAcounts()
-{
-    int temp = 0;
-    for (int i = 0; i < 50; i++)
-    {
-        if (bank[i][0] != 0)
-        {
-            temp = 1;
-            printf("The amount in account number %d is: %.2f\n", (i + SET_ACOUNT_NUMBER), bank[i][1]);
-        }
-    }
-    if (temp == 0)
-    {
-        printf("No accounts open.\n");
-    }
-}
-
-void closeAllAcounts()
-{
-=======
 void printAllOpenAccounts(){
      int temp = 0;
      for (int i = 0; i < 50; i++)
@@ -208,7 +100,6 @@ void printAllOpenAccounts(){
 }
 
 void closeAllAccounts(){
->>>>>>> 9ec81f2381bf461296a4ff0a71bd348e45c4bbc3
     for (size_t i = 0; i < 50; i++)
     {
         bank[i][0] = 0;
